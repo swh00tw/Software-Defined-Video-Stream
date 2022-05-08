fe283f381808cfda25f20a512c11bebf9a938cdd
swh00tw <a6140000@gmail.com>
2022-05-08 19:56:15 +0800

    add client code

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import multiprocessing as mp
 import os.path as osp
 import sys
 BUILD_DIR = osp.join(osp.dirname(osp.abspath(__file__)), "build/service/")
@@ -36,20 +37,24 @@ def gRPC_request(ip, port, isStart, algo):
         response = stub.Compute(request)
         print(response.value)
 
+def ffplay():
+    subprocess.run(['ffplay', '-fflags', 'nobuffer', 'rtmp://192.168.55.1/rtmp/live'])
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip", type=str, default="localhost")
     parser.add_argument("--port", type=int, default=8080)
     parser.add_argument("--algo", type=str, default="NONE")
     args = vars(parser.parse_args())
-    subprocess.run(['ffplay', '-fflags', 'nobuffer', 'rtmp://192.168.55.1/rtmp/live'])
-    # print(args)
-    # try:
-    #     # send gRPC request to tell server process start streaming
-    #     gRPC_request('192.168.55.1', 8080, 10, "NONE")
-    #     # run ffplay
-    #     subprocess.run(['ffplay', '-fflags', 'nobuffer', 'rtmp://192.168.55.1/rtmp/live'])
-    # except KeyboardInterrupt as e:
-    #     gRPC_request('192.168.55.1', 8080, 9, "NONE")
-    #     print("\nstop!!\n")
-    #     # send gPRC request to tell server process terminate streaming
+    # subprocess.run(['ffplay', '-fflags', 'nobuffer', 'rtmp://192.168.55.1/rtmp/live'])
+    print(args)
+    p1 = mp.Process(target=ffplay)
+    try:
+        # send gRPC request to tell server process start streaming
+        gRPC_request('192.168.55.1', 8080, 10, "NONE")
+        p1.start()
+    except KeyboardInterrupt as e:
+        gRPC_request('192.168.55.1', 8080, 9, "NONE")
+        p1.join()
+        print("\nstop!!\n")
+        # send gPRC request to tell server process terminate streaming
