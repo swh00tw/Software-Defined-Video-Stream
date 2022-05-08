d1105e9279441daa4845f58ac1091ff06f0bd7b1
swh00tw <a6140000@gmail.com>
2022-05-08 04:57:55 -0400

    finish client template

--- client.py
+++ client.py
@@ -10,7 +10,6 @@ import grpc
 import fib_pb2
 import fib_pb2_grpc
 
-
 def main(args):
     host = f"{args['ip']}:{args['port']}"
     print(host)
@@ -19,6 +18,20 @@ def main(args):
 
         request = fib_pb2.FibRequest()
         request.order = args['order']
+
+        response = stub.Compute(request)
+        print(response.value)
+
+# isStart = 1, start streaming
+# isStart > 1, end streaming
+def gRPC_request(ip, port, isStart, algo):
+    host = f"{ip}:{port}"
+    print(host)
+    with grpc.insecure_channel(host) as channel:
+        stub = fib_pb2_grpc.FibCalculatorStub(channel)
+
+        request = fib_pb2.FibRequest()
+        request.order = isStart
 
         response = stub.Compute(request)
         print(response.value)
@@ -42,8 +55,10 @@ if __name__ == "__main__":
     try:
         while True:
             # send gRPC request to tell server process start streaming
+            gRPC_request('192.168.55.1', '5000', 1, "NONE")
             # run ffplay
-            subprocess.run(['ffplay', '-fflags', 'nobuffer', 'rtmp://192.168.55.1/rtmp/live'])
+            # subprocess.run(['ffplay', '-fflags', 'nobuffer', 'rtmp://192.168.55.1/rtmp/live'])
     except KeyboardInterrupt as e:
+        gRPC_request('192.168.55.1', '5000', 3, "NONE")
         print("\nstop!!\n")
         # send gPRC request to tell server process terminate streaming
